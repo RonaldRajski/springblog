da085b107eb6a7044446bd0b498594e0b9de9616
Ronald Rajski <ronaldrajski@gmail.com>
2021-03-24 12:09:48 -0500

    example working no errors post lecture

--- src/main/java/com/codeup/codeup_demo/controllers/HelloController.java
+++ src/main/java/com/codeup/codeup_demo/controllers/HelloController.java
@@ -1,5 +1,11 @@
 package com.codeup.codeup_demo.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +27,7 @@ public class HelloController {
 
         model.addAttribute("aName", name.toUpperCase());
         model.addAttribute("admin", name.equals("fer"));
-        model.addAttribute("namelist", names);
+        model.addAttribute("nameList", names);
 
         return "hello";
 
