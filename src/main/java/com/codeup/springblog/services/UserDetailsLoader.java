cf9eda3f18dcf0aab4012fa455a0687c6241429c
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-01 11:56:04 -0500

    Created UserDetailsLoader and updated userRepository

--- src/main/java/com/codeup/springblog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/springblog/services/UserDetailsLoader.java
@@ -1,4 +1,30 @@
 package com.codeup.springblog.services;
 
-public class UserDetailsLoader {
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.models.UserWithRoles;
+import com.codeup.springblog.repo.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepository userDao;
+
+    public UserDetailsLoader(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
 }
+
