8d4e405c408927bc67a342935708b83daf46f66f
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-08 11:21:11 -0500

    modified tables-deleted main

--- src/main/java/com/codeup/codeup_demo/models/Image.java
+++ src/main/java/com/codeup/codeup_demo/models/Image.java
@@ -1,47 +1,55 @@
 package com.codeup.codeup_demo.models;
-//
-//import javax.persistence.*;
-//
-//import javax.persistence.*;
-//
-//@Entity
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="images" )
 public class Image {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(columnDefinition = "TEXT")
-//    private String path;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "ad_id")
-//    private Ad ad;
-//
-//    public Image() {
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getPath() {
-//        return path;
-//    }
-//
-//    public void setPath(String path) {
-//        this.path = path;
-//    }
-//
-//    public Ad getAd() {
-//        return ad;
-//    }
-//
-//    public void setAd(Ad ad) {
-//        this.ad = ad;
-//    }
+
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String path;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Image(Long id, String path, Post post) {
+        this.id = id;
+        this.path = path;
+        this.post = post;
+    }
+
+    public Image() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setAd(Post post) {
+        this.post = post;
+    }
 }
